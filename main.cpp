82471ddf7b40962fbfd614520be481cf2d495243
Matveev Stepan Andreevich <mset321@gmail.com>
2024-06-29 14:10:05 +0300

    лекция 7

--- main.cpp
+++ main.cpp
@@ -8,42 +8,18 @@ using std::cout;
 using std::endl;
 using std::cin;
 
-class A {
+class Base {
 public:
-    // virtual ~A() = default
-    virtual void hi() {
-        cout << "hi_A" << endl;
-    }
+    int x = 0;
 };
 
-class B : public A {
-public:
-    void hi() override {
-        cout << "hi_B" << endl;
-    }
-};
-
-class C : public A {
-public:
-    void hi() override {
-        cout << "hi_C" << endl;
-    }
-};
-
-
-
+class Derived : public Base {};
 
 int main() {
-    A* arr[2];
-    arr[0] = new B();
-    arr[1] = new C();
+    Derived derived;
+    Base& b = derived;
+    // Derived* d = &Base() так нельзя
+    static_cast<Derived&>(b);
 
-    arr[0]->hi();
-    arr[1]->hi(); 
-    
-    A &a = *arr[0];
-    B& b = dynamic_cast<B&>(a);
-    
-    b.hi();
 }
 
