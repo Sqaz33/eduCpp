a5a166c1aeb43782b236120468a662b7970c9f57
Matveev Stepan Andreevich <mset321@gmail.com>
2024-10-28 22:48:31 +0300

    feat: add full lectures 12

--- magistracy.cpp
+++ magistracy.cpp
@@ -819,6 +819,8 @@ int main() {
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
 
+//#### requires - хук в разрешение имен.
+
 
 // в requires может быть constexpr/consteval функция
 // для разрешения имен функции с requires - разные функции
@@ -1206,4 +1208,53 @@ int main() {
     std::cout << sum_allInt32(1, 2, 3, 4) << '\n';
     std::cout << sum_all(1, 2, 3, 4, 1) << '\n';
 
+}
+
+//----------------------------------
+// жадный матчинг
+template <typename T>
+class Stack {
+public:   
+    template <typename... Args>
+    void emplase_back(Args&&... args) {
+        top_ = std::make_unique<StackElem>(std::move(top_), std::forward<Args>(args)...); 
+    }   
+
+    template <typename... Args>
+    void push_back(const Args&... args) {
+        top_ = std::make_unique<StackElem>(std::move(top_), args...);
+    }
+private:
+    struct StackElem {
+        StackElem(std::unique_ptr<StackElem> nxt, T e) :
+            elem(e)
+            , next(std::move(nxt))
+        {}
+        
+        template <class... Args>
+        StackElem(std::unique_ptr<StackElem> nxt, Args&&... args) : // поменять местами пачку и указатель
+            elem(std::forward<Args>(args)...)
+            , next(std::move(nxt))
+        {}
+
+        T elem;
+        std::unique_ptr<StackElem> next;    
+    };
+
+private:
+    std::unique_ptr<StackElem> top_;
+
+};
+
+struct S {
+    S() { std::cout << "S()\n"; }
+    S(int) { std::cout << "S(int)\n"; }
+    S(const S&) { std::cout << "S(const S&)\n"; }
+    S(S&&) { std::cout << "S(S&&)\n"; }
+};
+
+
+int main() {   
+    Stack<S> s;
+    s.emplase_back(1);
 }
--- main.cpp
+++ main.cpp
@@ -1,22 +1,55 @@
 #include <concepts>
 #include <iostream>
 #include <utility>
+#include <memory>
 
 #include "things/same.hpp"
 
-template <class... Args>
-    requires msa::are_same_v<Args...>
-auto sum_all(Args&&... args) {
-    return (args + ...);
-}
 
-template <std::same_as<int>... Args>
-int sum_allInt32(Args&&... args) {
-    return (args + ...);
-} 
+// жадный матчинг
+template <typename T>
+class Stack {
+public:   
+    template <typename... Args>
+    void emplase_back(Args&&... args) {
+        top_ = std::make_unique<StackElem>(std::move(top_), std::forward<Args>(args)...); 
+    }   
+
+    template <typename... Args>
+    void push_back(const Args&... args) {
+        top_ = std::make_unique<StackElem>(std::move(top_), args...);
+    }
+private:
+    struct StackElem {
+        StackElem(std::unique_ptr<StackElem> nxt, T e) :
+            elem(e)
+            , next(std::move(nxt))
+        {}
+        
+        template <class... Args>
+        StackElem(std::unique_ptr<StackElem> nxt, Args&&... args) : // поменять местами пачку и указатель
+            elem(std::forward<Args>(args)...)
+            , next(std::move(nxt))
+        {}
+
+        T elem;
+        std::unique_ptr<StackElem> next;    
+    };
+
+private:
+    std::unique_ptr<StackElem> top_;
+
+};
+
+struct S {
+    S() { std::cout << "S()\n"; }
+    S(int) { std::cout << "S(int)\n"; }
+    S(const S&) { std::cout << "S(const S&)\n"; }
+    S(S&&) { std::cout << "S(S&&)\n"; }
+};
+
 
 int main() {   
-    std::cout << sum_allInt32(1, 2, 3, 4) << '\n';
-    std::cout << sum_all(1, 2, 3, 4, 1) << '\n';
-
+    Stack<S> s;
+    s.emplase_back(1);
 }
