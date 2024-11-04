c232621b2ab6ee729285170444c842022914c830
Matveev Stepan Andreevich <mset321@gmail.com>
2024-11-04 23:06:00 +0300

    feat: add lectures 17

--- main.cpp
+++ main.cpp
@@ -1,34 +1,21 @@
 #include <iostream>
 #include <type_traits>
-
-// namespace impl__ {
-
-// template <class T, class... Args>
-// constexpr decltype(std::declval<T>().foo(std::declval<Args>()...), true) 
-// has_method_foo_helper_(int) { return true; }
-
-// template <class T, class... Args>
-// constexpr bool has_method_foo_helper_(...) { return false; }
-
-// } // namespace impl__
-
-// template <class T, class... Args>
-// constexpr bool has_method_foo_v = impl__::has_method_foo_helper_<T, Args...>(1);
-
-// struct S { void foo(int) {} };
-
-
-
-
-template <class T, std::enable_if_t<(sizeof(T) >= 4)>* = nullptr>
-constexpr void maybe_int(T) { std::cout << "yes\n"; }
-
-template <class T, std::enable_if_t<(sizeof(T) < 4)>* = nullptr>
-constexpr void maybe_int(T) { std::cout << "no\n"; }
+#include <memory>
+#include <cassert>
 
 
 
 int main() {
-    maybe_int(1);
-    maybe_int('c');
+    std::weak_ptr<int> b;
+
+    {
+        auto a = std::make_shared<int>(1);
+        std::weak_ptr<int> b = a;
+
+        auto l = b.lock();
+    }
+
+    assert(b.expired());
+    assert(b.lock() == nullptr);
+
 }
