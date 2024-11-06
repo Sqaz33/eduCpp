fdda91990140202e695b7488ff72a9a03242187f
Matveev Stepan Andreevich <mset321@gmail.com>
2024-11-06 23:01:55 +0300

    feat: add lecture 19_2

--- main.cpp
+++ main.cpp
@@ -1,30 +1,56 @@
 #include <iostream>
-#include <type_traits>
 #include <thread>
+#include <signal.h>
 #include <mutex>
+#include <condition_variable>
+#include <ranges>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <functional>
 
-namespace prekol {
 
-struct Buf {
-    Buf() { std::cout << "Buf::Buf()\n"; }
-    ~Buf() { std::cout << "Buf::~Buf()\n"; }
-};
 
-void do_tmng() { throw std::bad_exception(); }
+// namespace {
 
-struct Impl : Buf {
-    Impl() : Buf() {
-        do_tmng();
-    }
-};
+// std::mutex gmut;
+// int counter = 0;
+// std::condition_variable data_cond;
 
-} // namespace prekol
+
+// void proc() {
+//     int c;
+//     std::unique_lock<std::mutex> lk{gmut};
+//     data_cond.wait(lk, [] { return counter > 0; }); 
+//     // при пробуждении и наступлении условия, 
+//     // лочить lk
+//     c = counter;
+// }
+
+
+// void preporation() {
+//     std::lock_guard<std::mutex> lk{gmut}; 
+//     ++counter;
+//     data_cond.notify_one(); // оповещает другой поток.
+// }
+
+// } // namespace
+
+// int x = 0;
+// std::mutex xmut;
+
 
 int main() {
-    using namespace prekol;
+    std::vector<std::pair<int, int>> v = { {0, 1}, {0, 4}, {0, 2}, {0, 3}, {0, 0} };
 
-    try { Impl i; } catch(...) { }
-    // on display
-    // Buf::Buf() 
-    // Buf::~Buf()
+    // std::sort(v.begin(), v.end(),
+    //      [](const auto& a, const auto& b) { return a.second < b.second; });
+
+    std::ranges::sort(v, {}, std::mem_fn(&std::pair<int, int>::second));
+
+
+    for (const auto& [f, s] : v) {
+        std::cout << f << ' ' << s << '\n';
+    }
+
 }
