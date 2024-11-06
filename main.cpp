f9f1a5dd54260ecb491db1016186d5e72e54797c
Matveev Stepan Andreevich <mset321@gmail.com>
2024-11-06 12:23:34 +0300

    feat: start lectures 19_1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
+#include <thread>
+#include <mutex>
+
+// RAII синхронизация
+
+namespace {
+
+void __attribute__((noinline)) use(int c) { asm(""); }  
+
+int x;
+std::mutex mforx;
+
+void race() {
+    for (int i = 0; i < 1'000'000; ++i) { 
+        std::lock_guard<std::mutex> lk(mforx);
+        x += 1; use(x); 
+    }
+    for (int i = 0; i < 1'000'000; ++i) { 
+        std::lock_guard<std::mutex> lk(mforx);
+        x -= 1; use(x); 
+    }
+} 
+
+} // namespace
 
 int main() {
-    
+    std::thread t1{race};
+    std::thread t2{race};
+
+    t1.join();
+    t2.join(); 
+
+    std::cout << x << std::endl; 
 }
