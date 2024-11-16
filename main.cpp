707a278c44adff99dcd3777071c567e99788331d
Matveev Stepan Andreevich <mset321@gmail.com>
2024-11-16 18:32:57 +0300

    feat: add some new topics

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <compare>
+#include <generator>
 
 
+std::generator<int> lessTen() {
+    int n = 0;
+    for (;;) {
+        if (n > 10) break;
+        co_yield n;
+        n += 1;
+    }
+}
 
 int main() {
-    int* arr = new int[2];
-    int x = arr[2];
+    for (auto n : lessTen()) {
+        std::cout << n << ' ';
+    }
+    std::cout << std::endl;
 }
